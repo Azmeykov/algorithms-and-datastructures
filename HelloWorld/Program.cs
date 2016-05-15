ea6c02bad328d86865b55645d148a256ea8c7f2c
Aleksey Zmeykov <Aleksey Zmeykov>
2016-05-15 23:29:03 +0300

    int

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,15 +12,38 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+
             //ввод 2-х чисел и вывод суммы
             //найти типы данных для C#
             //объявить целочисленную переменную
-            int a, b;
             //ввести в переменную a и b значения
             //сложить а и b
             //сохранить результат в третью переменную
             //вывести результат на экран
+            int a, b;
+            string buff = Console.ReadLine();
+            a = Convert.ToInt16(buff);
+            Console.WriteLine("a: " + a);
+            buff = Console.ReadLine();
+            b = Convert.ToInt16(buff);
+            Console.WriteLine("b: " + b);
+            //TODO: Написать ввод третьего значения d;
+            //TODO: Вывести сумму трех значений;
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("Values: " + a + ", " + b + ";");            
+            string str = "TestString";
+            Console.WriteLine(str + (a + b));
+
+            double x = 1 + 0.5;
+            Console.WriteLine(x);
+
+
+            Console.ReadKey();
         }
+        
     }
 }
